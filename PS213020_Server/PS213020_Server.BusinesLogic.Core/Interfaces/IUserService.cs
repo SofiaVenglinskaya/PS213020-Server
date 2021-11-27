4f9a89625495f23d809ac461cd435484575c1ab9
yourboyfriendsdrug <74487159+yourboyfriendsdrug@users.noreply.github.com>
2021-11-27 17:50:51 +0300

    Сервисы

---
+++ PS213020_Server/PS213020_Server.BusinesLogic.Core/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using PS213020_Server.BusinesLogic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS213020_Server.BusinesLogic.Core.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserInformationBlo> Auth(int phoneNumberPrefix, int phoneNumber, string password);
+        Task<UserInformationBlo> Registration(int phoneNumberPrefix, int phoneNumber, string password);
+        Task<UserInformationBlo> Get(int UserId);
+        Task<UserInformationBlo> Update(int phoneNumberPrefix, int phoneNumber, string password, UserUpdateBlo userUpdateBlo);
+        Task<bool> DoesExist(int phoneNumberPrefix, int phoneNumber);
+    }
+
+}
---
+++ PS213020_Server/PS213020_Server.BusinesLogic/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using PS213020_Server.BusinesLogic.Core.Interfaces;
+using PS213020_Server.BusinesLogic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS213020_Server.BusinesLogic.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<UserInformationBlo> Auth(int phoneNumberPrefix, int phoneNumber, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DoesExist(int phoneNumberPrefix, int phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInformationBlo> Get(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInformationBlo> Registration(int phoneNumberPrefix, int phoneNumber, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInformationBlo> Update(int phoneNumberPrefix, int phoneNumber, string password, UserUpdateBlo userUpdateBlo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
